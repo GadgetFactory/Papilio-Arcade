5b3afdb39c8378479dd4f644d566d1011234b2c1
vlait <vlaitine@gmail.com>
2015-02-07 17:59:27 +0200

    Revert "keyboard controls, builds but untested"
    This reverts commit ddc5bf1ce27a7b7fe580b7f33f1758ac62dbc2df.

--- A2601/A2601/src/A2601NoFlash.vhd
+++ A2601/A2601/src/A2601NoFlash.vhd
@@ -33,15 +33,13 @@ use ieee.numeric_std.all;
 
 entity A2601NoFlash is
    port (clk: in std_logic;
-			O_AUDIO_R: out std_logic;
-			O_AUDIO_L: out std_logic;
-			
+			audio: out std_logic;
          O_VSYNC: out std_logic;
          O_HSYNC: out std_logic;
 			O_VIDEO_R: out std_logic_vector(3 downto 0);
 			O_VIDEO_G: out std_logic_vector(3 downto 0);
 			O_VIDEO_B: out std_logic_vector(3 downto 0);			
-
+--         au: out std_logic_vector(4 downto 0);
          res: in std_logic;
          p_l: in std_logic;
          p_r: in std_logic;
@@ -55,14 +53,11 @@ entity A2601NoFlash is
          p2_d: in std_logic;
 			
          p_s: in std_logic;
-
          p_bs: out std_logic;			
-
 			LED: out std_logic_vector(2 downto 0);
 			
 			I_SW :in std_logic_vector(2 downto 0);
-			PS2CLK1   : inout	std_logic;
-			PS2DAT1   : inout	std_logic;
+			
 			
          JOYSTICK_GND: out std_logic			;
 			JOYSTICK2_GND: out std_logic	
@@ -71,63 +66,128 @@ end A2601NoFlash;
 
 architecture arch of A2601NoFlash is
 
-	--
-	-- bank switch 
-	subtype bss_type is std_logic_vector(2 downto 0);
+	COMPONENT a2601_dcm
+	PORT(
+		CLKIN_IN : IN std_logic; 
+		RST_IN : IN std_logic; 		
+		CLKFX_OUT : OUT std_logic;
+		CLKIN_IBUFG_OUT : OUT std_logic;
+		CLK0_OUT : OUT std_logic;
+		CLK2X_OUT : OUT std_logic
+		);
+	END COMPONENT;
+
+    component A2601 is
+    port(vid_clk: in std_logic;
+         rst: in std_logic;
+         d: inout std_logic_vector(7 downto 0);
+         a: out std_logic_vector(12 downto 0);
+			r: out std_logic;
+         pa: inout std_logic_vector(7 downto 0);
+         pb: inout std_logic_vector(7 downto 0);
+         inpt4: in std_logic;
+         inpt5: in std_logic;
+         colu: out std_logic_vector(6 downto 0);
+         csyn: out std_logic;
+         vsyn: out std_logic;
+         hsyn: out std_logic;
+			rgbx2: out std_logic_vector(23 downto 0);
+         cv: out std_logic_vector(7 downto 0);
+         au0: out std_logic;
+         au1: out std_logic;
+         av0: out std_logic_vector(3 downto 0);
+         av1: out std_logic_vector(3 downto 0);
+         ph0_out: out std_logic;
+         ph1_out: out std_logic);
+    end component;
+
+	COMPONENT cart_rom
+	PORT(
+		clk : IN std_logic;
+		addr : IN std_logic_vector(13 downto 0);          
+		data : OUT std_logic_vector(7 downto 0)
+		);
+	END COMPONENT;
 	
-	constant BANK00: bss_type := "000";
-	constant BANKF8: bss_type := "001";
-	constant BANKF6: bss_type := "010";
-	constant BANKFE: bss_type := "011";
-	constant BANKE0: bss_type := "100";
-	constant BANK3F: bss_type := "101";
+	COMPONENT VGA_SCANDBL
+	PORT(
+		I_R : IN std_logic_vector(2 downto 0);
+		I_G : IN std_logic_vector(2 downto 0);
+		I_B : IN std_logic_vector(1 downto 0);
+		I_HSYNC : IN std_logic;
+		I_VSYNC : IN std_logic;
+		CLK : IN std_logic;
+		CLK_X2 : IN std_logic;          
+		O_R : OUT std_logic_vector(2 downto 0);
+		O_G : OUT std_logic_vector(2 downto 0);
+		O_B : OUT std_logic_vector(1 downto 0);
+		O_HSYNC : OUT std_logic;
+		O_VSYNC : OUT std_logic
+		);
+	END COMPONENT;	
 	
-	-- bank switch and superchip enable options.
-	
-	signal bss: bss_type := BANK00; 	--bank switching method
-	signal sc: std_logic := '0';		--superchip enabled or not
+	 component dac is
+	 port(DACout: 	out std_logic;
+			DACin:	in std_logic_vector(4 downto 0);
+			Clk:		in std_logic;
+			Reset:	in std_logic);
+	 end component;	
 	 
-	 
+   component ram128x8 is
+        port(clk: in std_logic;
+             r: in std_logic;
+             d_in: in std_logic_vector(7 downto 0);
+             d_out: out std_logic_vector(7 downto 0);
+             a: in std_logic_vector(6 downto 0));
+    end component;
+
+    component CartTable is
+        port(clk: in std_logic;
+             d: out std_logic_vector(10 downto 0);
+             c: out std_logic_vector(6 downto 0);
+             a: in std_logic_vector(6 downto 0));
+    end component;
     
-	signal vid_clk, vid_clkx2: std_logic;
-	signal d: std_logic_vector(7 downto 0);
-	--signal cpu_d: std_logic_vector(7 downto 0);
-	signal a: std_logic_vector(13 downto 0);
-	signal pa: std_logic_vector(7 downto 0);
-	signal pb: std_logic_vector(7 downto 0);
-	signal inpt4: std_logic;
-	signal inpt5: std_logic;
-	signal colu: std_logic_vector(6 downto 0);
-	signal csyn: std_logic;
-	signal audio: std_logic;
-	signal au0: std_logic;
-	signal au1: std_logic;
-	signal av0: std_logic_vector(3 downto 0);
-	signal av1: std_logic_vector(3 downto 0);
+	 signal vid_clk, vid_clkx2: std_logic;
+    signal d: std_logic_vector(7 downto 0);
+    --signal cpu_d: std_logic_vector(7 downto 0);
+    signal a: std_logic_vector(13 downto 0);
+    signal pa: std_logic_vector(7 downto 0);
+    signal pb: std_logic_vector(7 downto 0);
+    signal inpt4: std_logic;
+    signal inpt5: std_logic;
+    signal colu: std_logic_vector(6 downto 0);
+    signal csyn: std_logic;
+    signal au0: std_logic;
+    signal au1: std_logic;
+    signal av0: std_logic_vector(3 downto 0);
+    signal av1: std_logic_vector(3 downto 0);
 
-	signal auv0: unsigned(4 downto 0);
-	signal auv1: unsigned(4 downto 0);
+    signal auv0: unsigned(4 downto 0);
+    signal auv1: unsigned(4 downto 0);
 
-	signal rst: std_logic := '1';
-	signal sys_clk_dvdr: unsigned(4 downto 0) := "00000";
+    signal rst: std_logic := '1';
+    signal sys_clk_dvdr: unsigned(4 downto 0) := "00000";
 
-	signal ph0: std_logic;
-	signal ph1: std_logic;
+    signal ph0: std_logic;
+    signal ph1: std_logic;
 	 
-	signal rgbx2: std_logic_vector(23 downto 0);
-	signal hsyn: std_logic;
-	signal vsyn: std_logic;
-	signal ctrl_cntr: unsigned(3 downto 0);
-	signal p_fn: std_logic;
+    signal rgbx2: std_logic_vector(23 downto 0);
+    signal hsyn: std_logic;
+    signal vsyn: std_logic;
+	 
+	 signal ctrl_cntr: unsigned(3 downto 0);
+	 signal gsel: std_logic;
+    signal p_fn: std_logic;
 
-	signal rst_cntr: unsigned(12 downto 0) := "0000000000000";
-	signal sc_clk: std_logic;
-	signal sc_r: std_logic;
-	signal sc_d_in: std_logic_vector(7 downto 0);
-	signal sc_d_out: std_logic_vector(7 downto 0);
-	signal sc_a: std_logic_vector(6 downto 0);
+		signal rst_cntr: unsigned(12 downto 0) := "0000000000000";
+		   signal sc_clk: std_logic;
+    signal sc_r: std_logic;
+    signal sc_d_in: std_logic_vector(7 downto 0);
+    signal sc_d_out: std_logic_vector(7 downto 0);
+    signal sc_a: std_logic_vector(6 downto 0);
 
-	
+    subtype bss_type is std_logic_vector(2 downto 0);
 
 
     signal bank: std_logic_vector(3 downto 0) := "0000";
@@ -141,69 +201,81 @@ architecture arch of A2601NoFlash is
     signal cpu_d: std_logic_vector(7 downto 0);
     signal cpu_r: std_logic;
 
-    
+    signal cart_info: std_logic_vector(10 downto 0) := "00000000000";
+    signal cart_cntr: unsigned(6 downto 0) := "0000000";
+    signal cart_max: std_logic_vector(6 downto 0);
+    signal cart_vect: std_logic_vector(6 downto 0) := "0000000";
+
+    signal cart_next: std_logic;
+    signal cart_prev: std_logic;
+    signal cart_swch: std_logic := '0';
+    signal cart_next_l: std_logic;
+    signal cart_prev_l: std_logic;
+	 signal sel: std_logic;
+--	 signal ctrl_cntr: unsigned(3 downto 0);
+	--tmp
 	signal cv:  std_logic_vector(7 downto 0);
 	signal au:  std_logic_vector(4 downto 0);
 -- switches
 	signal sw_toggle: std_logic_vector(2 downto 0) := "000";
+	
 	signal sw_pressed: std_logic_vector(2 downto 0) := "000";
 	
+    constant BANK00: bss_type := "000";
+    constant BANKF8: bss_type := "001";
+    constant BANKF6: bss_type := "010";
+    constant BANKFE: bss_type := "011";
+    constant BANKE0: bss_type := "100";
+    constant BANK3F: bss_type := "101";
 
-	-- ps/2  
-	signal ps2_codeready      : std_logic := '1';
-	signal ps2_scancode       : std_logic_vector( 9 downto 0) := (others => '0');
-	signal P1_CSJUDLR         : std_logic_vector( 6 downto 0) := (others => '0'); -- player 1 keyboard controls
-	signal P2_CSJUDLR         : std_logic_vector( 6 downto 0) := (others => '0'); -- player 2 keyboard controls
+    signal bss: bss_type := BANK00; 	--bank switching method
+    signal sc: std_logic := '0';		--superchip enabled or not
+	 
 begin
 	  
+--	Inst_a2601_dcm: a2601_dcm PORT MAP(
+--		CLKIN_IN => clk,
+--		RST_IN => '0',		
+--		CLKFX_OUT => vid_clk,
+--		CLKIN_IBUFG_OUT => open,
+--		CLK0_OUT => open,
+--		CLK2X_OUT => vid_clkx2
+--	);		
 
+  u_clocks : entity work.PACMAN_CLOCKS
+    port map (
+      I_CLK_REF  => clk,
+      I_RESET_L  => '1',
+      --
+      O_CLK_REF  => open,
+      --
+      O_ENA_12   => open,
+      O_ENA_6    => open,
+      O_CLK      => vid_clk,
+      O_RESET    => open
+      );	  
 
-	u_clocks : entity work.PACMAN_CLOCKS
-		port map (
-		I_CLK_REF  => clk,
-		I_RESET_L  => '1',
-		--
-		O_CLK_REF  => open,
-		--
-		O_ENA_12   => open,
-		O_ENA_6    => open,
-		O_CLK      => vid_clk,
-		O_RESET    => open
-		);	  
-
-
-	ms_A2601: entity work.A2601
-		port map(vid_clk, rst, cpu_d, cpu_a, cpu_r,pa, pb, inpt4, inpt5, colu, csyn, vsyn, hsyn, rgbx2, cv, au0, au1, av0, av1, ph0, ph1);
+    ms_A2601: A2601
+        port map(vid_clk, rst, cpu_d, cpu_a, cpu_r,pa, pb, inpt4, inpt5, colu, csyn, vsyn, hsyn, rgbx2, cv, au0, au1, av0, av1, ph0, ph1);
   
-	O_VIDEO_R(3 downto 1) <= rgbx2(23 downto 21);
-	O_VIDEO_R(0) <= '0';
-	O_VIDEO_G(3 downto 1) <= rgbx2(15 downto 13);
-	O_VIDEO_G(0) <= '0';
-	O_VIDEO_B(3 downto 2) <= rgbx2(7 downto 6);	
-	O_VIDEO_B(1 downto 0) <= "00";
-	O_HSYNC   <= hsyn;
-	O_VSYNC   <= vsyn;	
-	
-	
-	dac_inst: entity work.dac 
-		port map(audio, au, vid_clk, '0');	
-
-		auv0 <= ("0" & unsigned(av0)) when (au0 = '1') else "00000";
-		auv1 <= ("0" & unsigned(av1)) when (au1 = '1') else "00000";
-		au <= std_logic_vector(auv0 + auv1);
-		
-		O_AUDIO_R <= audio;
-		O_AUDIO_L <= audio;
-
-
-
-
-	Inst_cart_rom: entity work.cart_rom PORT MAP(
+	Inst_cart_rom: cart_rom PORT MAP(
 		clk => vid_clk,
 		data => d,
 		addr => a
 	);		 
+	--brd_CartTable: CartTable
+   --     port map(ph0, cart_info, cart_max, std_logic_vector(cart_cntr));
 	
+	dac_inst: dac 
+		port map(audio, au, vid_clk, '0');	
+
+	
+      O_VIDEO_R(3 downto 1) <= rgbx2(23 downto 21);
+      O_VIDEO_G(3 downto 1) <= rgbx2(15 downto 13);
+      O_VIDEO_B(3 downto 2) <= rgbx2(7 downto 6);	
+      O_HSYNC   <= hsyn;
+      O_VSYNC   <= vsyn;	
+
 --    cpu_d <= d when a(12) = '1' else "ZZZZZZZZ";
  process(ph0)
     begin
@@ -220,7 +292,9 @@ begin
     -- Controller inputs sampling
     p_bs <= '0'; --- led0 ctrl_cntr(3);
 
-    
+    -- Only one controller port supported. -- FIXME -- does 2nd port work ?
+    -- pa(3 downto 0) <= "1111";
+	 
     process(ph0)
     begin
         if (ph0'event and ph0 = '1') then
@@ -229,11 +303,11 @@ begin
                 p_fn <=  p_a;
                 pb(0) <= not p_s; 
             elsif (ctrl_cntr = "0111") then
-                pa(7 downto 4) <=  (p_r and P1_CSJUDLR(0)) & (p_l and P1_CSJUDLR(1)) & (p_d and P1_CSJUDLR(2)) &  ( p_u and P1_CSJUDLR(3));
+                pa(7 downto 4) <= p_r & p_l & p_d & p_u;
 					 pa(3 downto 0) <= p2_r & p2_l & p2_d & p2_u;
-                inpt4 <=  (p_a and P1_CSJUDLR(4));
+                inpt4 <= p_a;
 					 inpt5 <= p2_a;
-                
+                gsel <= not p_s;
 					 
 					 --switches
 					 for i in 0 to 1
@@ -259,12 +333,12 @@ begin
 	 pb(7) <= sw_toggle(0);
 	 pb(6) <= sw_toggle(1);
 	 pb(1) <= sw_toggle(2);
-	 pb(3) <= '1'; --1 colour / 0 bw
+	 pb(3) <= '0'; --not used
     pb(5) <= '1'; --nc ?
     pb(4) <= '1'; --nc
     pb(2) <= '1'; --nc
 	 
-    
+    -- inpt5 <= '1'; --FIXME --remove line if 2p works
 	 -- leds to show state of switches
 	 -- led1 difficulty switch 1 on/off
 	 -- led2 difficulty switch 2 on/off
@@ -275,10 +349,22 @@ begin
 	 JOYSTICK_GND <= '0';
 	 JOYSTICK2_GND <= '0';
 	 
+    auv0 <= ("0" & unsigned(av0)) when (au0 = '1') else "00000";
+    auv1 <= ("0" & unsigned(av1)) when (au1 = '1') else "00000";
 
-   
+    au <= std_logic_vector(auv0 + auv1);
 
-    sc_ram128x8: entity work.ram128x8
+    --process(vid_clk, sys_clk_dvdr)
+    --begin
+     --   if (vid_clk'event and vid_clk = '1') then
+     --       sys_clk_dvdr <= sys_clk_dvdr + 1;
+     --       if (sys_clk_dvdr = "11101") then
+     --           rst <= '0';
+     --       end if;
+     --   end if;
+    --end process;
+
+    sc_ram128x8: ram128x8
         port map(sc_clk, sc_r, sc_d_in, sc_d_out, sc_a);
 
     -- This clock is phase shifted so that we can use Xilinx synchronous block RAM.
@@ -311,13 +397,20 @@ begin
     tf_bank <= bank(1 downto 0) when (cpu_a(11) = '0') else "11";
 
     with bss select a <=
-		"00" & cpu_a(11 downto 0) 					when BANK00,
-		'0' & bank(0) & cpu_a(11 downto 0) 		when BANKF8,
-		bank(1 downto 0) & cpu_a(11 downto 0) 	when BANKF6,
-		'0' & bank(0) & cpu_a(11 downto 0) 		when BANKFE,
-      "0" & e0_bank & cpu_a(9 downto 0) 		when BANKE0,
-		'0' & tf_bank & cpu_a(10 downto 0) 		when BANK3F,
-		"--------------" when others;
+        --cart_vect & 
+		  "00" & cpu_a(11 downto 0) when BANK00,
+        --cart_vect(6 downto 1) & 
+		  '0' & bank(0) & cpu_a(11 downto 0) when BANKF8,
+        --cart_vect(6 downto 2) & 
+		  bank(1 downto 0) & cpu_a(11 downto 0) when BANKF6,
+        --cart_vect(6 downto 1) & 
+		  '0' & bank(0) & cpu_a(11 downto 0) when BANKFE,
+        --cart_vect(6 downto 1) & 
+		  "0" & e0_bank & cpu_a(9 downto 0) when BANKE0,
+        --cart_vect(6 downto 1) & 
+		  '0' & tf_bank & cpu_a(10 downto 0) when BANK3F,
+        --"-------------------" when others;
+		  "--------------" when others;
 
     bankswch: process(ph0)
     begin
@@ -371,58 +464,12 @@ begin
         end if;
     end process;
 
-   
-	inst_kbd : entity work.Keyboard
-		generic map (clk_freq => 25) 
-		port map (
-			Reset     => '0',
-			Clock     => vid_clk,
-			PS2Clock  => PS2CLK1,
-			PS2Data   => PS2DAT1,
-			CodeReady => ps2_codeready,  --: out STD_LOGIC;
-			ScanCode  => ps2_scancode    --: out STD_LOGIC_VECTOR(9 downto 0)
-		);
-
-
-	--	http://www.computer-engineering.org/ps2keyboard/scancodes2.html
-	-- ScanCode(9)          : 1 = Extended  0 = Regular
-	-- ScanCode(8)          : 1 = Break     0 = Make
-	-- ScanCode(7 downto 0) : Key Code
-	process(vid_clk)
-	begin
-		if rising_edge(vid_clk) then
-			if rst = '1' then
-				P1_CSJUDLR <= (others=>'1'); --active low inputs
-				P2_CSJUDLR <= (others=>'1');
-			elsif (ps2_codeready = '1') then
-				case (ps2_scancode(7 downto 0)) is
-					--when x"05" =>	P1_CSJUDLR(6) <=  ps2_scancode(8);     -- P1 coin "F1"
-					--when x"04" =>	P2_CSJUDLR(6) <=  ps2_scancode(8);     -- P2 coin "F3"
-
-					--when x"06" =>	P1_CSJUDLR(5) <=  ps2_scancode(8);     -- P1 start "F2"
-					--when x"0c" =>	P2_CSJUDLR(5) <=  ps2_scancode(8);     -- P2 start "F4"
-
-					when x"14" =>	P1_CSJUDLR(4) <=  ps2_scancode(8);     -- P1 jump "LCTRL"
-										--P2_CSJUDLR(4) <=  ps2_scancode(8);     -- P2 jump "I"
-
-					when x"75" =>	P1_CSJUDLR(3) <=  ps2_scancode(8);     -- P1 up arrow
-										--P2_CSJUDLR(3) <=  ps2_scancode(8);     -- P2 up arrow
-
-					when x"72" =>	P1_CSJUDLR(2) <=  ps2_scancode(8);     -- P1 down arrow
-										--P2_CSJUDLR(2) <=  ps2_scancode(8);     -- P2 down arrow
-
-					when x"6b" =>	P1_CSJUDLR(1) <=  ps2_scancode(8);     -- P1 left arrow
-										--P2_CSJUDLR(1) <=  ps2_scancode(8);     -- P2 left arrow
-
-					when x"74" =>	P1_CSJUDLR(0) <=  ps2_scancode(8);     -- P1 right arrow
-										--P2_CSJUDLR(0) <=  ps2_scancode(8);     -- P2 right arrow
-
-					when others => null;
-				end case;
-			end if;
-		end if;
-	end process;
-	 
+    --bss <= cart_info(3 downto 1);
+    --sc <= cart_info(0);
+    --cart_vect <= cart_info(10 downto 4);
+	--LED <= bss;
+    --cart_next <= (not pa(7)) and (not gsel);
+    --cart_prev <= (not pa(6)) and (not gsel);
 end arch;
 
 
